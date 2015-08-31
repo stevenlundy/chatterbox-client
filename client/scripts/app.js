90a37724d597e1fcff68a543f565b83fd3ce480d
stevenlundy <steven.lundy@gmail.com>
2015-08-31 14:39:50 -0700

    wrote ajax request for chat messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,25 @@
 // YOUR CODE HERE:
+var parseUrl = 'https://api.parse.com/1/classes/chatterbox';
+
+$.ajax({
+  url: parseUrl,
+  type: 'GET',
+  success: function(data){
+    console.log(data);
+    // $('#chats').append($('<div>'+encodeHTMLEntities(data.results[86].text)+'</div>'))
+    _.each(data.results, function(message){
+      if(message.text){
+        $('#chats').append($('<div class="card-panel teal lighten-2"/>').text(message.text));
+      }
+    });
+  },
+  error: function(){
+    console.error('error');
+  }
+});
+
+
+var encodeHTMLEntities = function(string) {
+  string = ''+string;
+  return string.replace(/\<\W*script[^>]*\>/gi, '');
+};
